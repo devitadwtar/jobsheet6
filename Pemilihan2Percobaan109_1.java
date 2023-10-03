2028e4799e7ce6e7f93a7a215ef2766d6911eedd
devitadwtar <144086102+devitadwtar@users.noreply.github.com>
2023-10-03 12:40:46 +0700

    Add files via upload

---
+++ Pemilihan2Percobaan109_1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan109_1 {
+    public static void main(String[] args){
+       
+        Scanner input09 = new Scanner (System.in);
+        System.out.println("Masukkan tahun");
+        int tahun = input09.nextInt();
+
+        if ((tahun % 4) == 0){
+            if ((tahun % 100) !=0) {
+                System.out.println("Tahun kabisat");
+            } else if ((tahun % 400) == 0) {
+            System.out.println("Tahun kabisat");
+            } else if ((tahun % 100) == 0) {
+                System.out.println("Bukan tahun kabisat");
+            } else if ((tahun % 100) == 0) {
+            }
+    } else
+        System.out.println("Bukan tahun kabisat");
+}
+}
